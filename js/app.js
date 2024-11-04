0d7b2cd3ee3c056af7a4a72f68241017142886d6
Alvaro Bedoya <75597515+Russelt30@users.noreply.github.com>
2024-11-03 19:00:07 -0500

    carrusel

--- js/app.js
+++ js/app.js
@@ -1,2 +1,39 @@
-const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
-const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
+const tooltipTriggerList = document.querySelectorAll(
+  '[data-bs-toggle="tooltip"]'
+);
+const tooltipList = [...tooltipTriggerList].map(
+  (tooltipTriggerEl) => new bootstrap.Tooltip(tooltipTriggerEl)
+);
+
+// vegetable carousel
+$(".vegetable-carousel").owlCarousel({
+  autoplay: true,
+  smartSpeed: 1500,
+  center: false,
+  dots: true,
+  loop: true,
+  margin: 25,
+  nav: true,
+  navText: [
+    '<i class="bi bi-arrow-left"></i>',
+    '<i class="bi bi-arrow-right"></i>',
+  ],
+  responsiveClass: true,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    576: {
+      items: 1,
+    },
+    768: {
+      items: 2,
+    },
+    992: {
+      items: 3,
+    },
+    1200: {
+      items: 4,
+    },
+  },
+});
